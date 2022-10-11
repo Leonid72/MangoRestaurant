64ffb313b5b3054e383113135f98965fc8e50ad7
Leonid Nisanov <leonid72@gmail.com>
2022-10-11 06:03:53 +0300

    CartIndex Added

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+        public CartController(IProductService service, ICartService cartService)
+        {
+            _productService = service;
+            _cartService = cartService;
+        }
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await LoadCartDtoBaseOnLoggedInUser());
+        }
+
+        private async Task<CartDto> LoadCartDtoBaseOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.GetCartByUserAsync<ResponseDto>(userId, accessToken);
+            CartDto cartDto = new();
+            if (response.IsSuccess && response != null)
+                cartDto = JsonConvert.DeserializeObject<CartDto>(response.Result.ToString());
+            if (cartDto.CartHeader != null)
+            {
+                foreach (var detail in cartDto.CartDetails)
+                {
+                    cartDto.CartHeader.OrderTotal += detail.Product.Price * detail.Count;
+                }
+            }
+
+            return cartDto;
+        }
+
+        public async Task<IActionResult> Remove(int cartDetailsId)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.RemoveFromCartAsync<ResponseDto>(cartDetailsId, accessToken);
+            if (response != null && response.IsSuccess)
+                return  RedirectToAction(nameof(CartIndex));
+
+            return View();
+        }
+    }
+}
