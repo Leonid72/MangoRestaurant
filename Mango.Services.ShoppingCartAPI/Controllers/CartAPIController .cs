edbb542ed26b2fcc230a1303f5922cacd7fdd7d3
Leonid Nisanov <leonid72@gmail.com>
2022-11-11 00:51:13 +0200

    1212

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController .cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController .cs
@@ -167,7 +167,7 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                 checkoutHeaderDto.CartDetails = cartDto.CartDetails;
                 //await _messageBus.PublishMessage(checkoutHeaderDto, _configuration["AzureServiceBus:TopicName"]);
                 _rabbitMQCartMessage.SendMessage(checkoutHeaderDto, "checkoutqueue");
-                _cartRepository.ClearCart(checkoutHeaderDto.UserId);
+                await _cartRepository.ClearCart(checkoutHeaderDto.UserId);
             }
             catch (Exception ex)
             {
